776f2d3e91ffb46567ab9ee1ff79966604b812d3
alcalino-git <alanhumber333@gmail.com>
2025-03-22 19:25:49 -0600

    Revert "idonevenknowanymore"
    This reverts commit 28b50573e04d8ada20eaf26c0c4223885b618b84.

--- .vercel/output/functions/_render.func/dist/server/chunks/evidencias_CtGhvmbH.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./evidencias_Cqv7Rhwn.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- .vercel/output/functions/_render.func/dist/server/chunks/fuentes_DgPk0gTt.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./fuentes_B0azO7XA.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
